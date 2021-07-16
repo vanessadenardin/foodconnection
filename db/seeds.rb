d7586fdee53e477ef25ea9797f3abd1b3f60e4c5
suztopp <suzannetopp81@gmail.com>
2021-07-16 17:04:18 +1000

    fixing seeding issues with adding recipe for user1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 puts 'destroying old records'
 
@@ -13,37 +6,62 @@ User.destroy_all
 puts 'seeding started'
 puts 'creating user 1'
 
-user1 = User.create(username: 'testaccount', email: 'test@test.com', password_digest: '123456')
+user1 = User.create(username: 'testaccount', email: 'test@test.com', password: "123456", password_confirmation: "123456")
 
 puts "Created User"
 puts 'Creating recipes user 1'
 
+# recipe = 
+# 3.times do 
+# 	user1.recipes.create(
+recipe = Recipe.create(
+    recipe_name: "test1",
+    recipe_instructions: "test1test1test1test1test1",
+    cooking_time: 30,
+    serves: 2,
+    skill_level: "beginners",
+    cuisine: "Indian",
+    meal_type: "breakfast",
+    user: User.last
+)
+
+recipe = Recipe.create(
+    recipe_name: "test2",
+    recipe_instructions: "test2test2test2test2test2",
+    cooking_time: 30,
+    serves: 2,
+    skill_level: "beginners",
+    cuisine: "Indian",
+    meal_type: "breakfast",
+    user: User.last
+)
+
+recipe = Recipe.create(
+    recipe_name: "test3",
+    recipe_instructions: "test3test3test3test3test3",
+    cooking_time: 30,
+    serves: 2,
+    skill_level: "beginners",
+    cuisine: "Indian",
+    meal_type: "breakfast",
+    user: User.last
+)
+
+
+puts recipe.errors.full_messages unless recipe.valid?
+
 # Recipe.create(
-#     recipe_name: "test",
-#     recipe_instructions: "hello",
-#     cooking_time: 2,
-#     serves: 2,
-#     skill_level: "beginners",
-#     cuisine: "Indian",
-#     meal_type: "breakfast",
-#     user: User.last
+#     recipe_name: "test2",
+#     recipe_instructions: "test2",
+#     cooking_time: 60,
+#     serves: 4,
+#     skill_level: "intermediate",
+#     cuisine: "Mexican",
+#     meal_type: "lunch",
+#     user_id: User.last
 # )
 
-# puts "Created Recipe!"
-
-3.times do 
-	user1.recipes.create(
-	recipe_name: ['test recipe', 'testy recipe', 'testytesty recipe'].sample,
-	recipe_instructions: ['Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...', 
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec purus magna. Curabitur at venenatis ipsum. Phasellus pulvinar est vel dolor tempor tristique.', 
-        'Aliquam erat volutpat. Phasellus pharetra posuere sem, eget scelerisque lorem auctor sollicitudin. Mauris lobortis mollis risus ut tempor.'].sample,
-	cooking_time: [1,2,3,4].sample,
-	serves: [1,2,4,6].sample,
-    skill_level: ['beginners', 'intermediate', 'advanced', 'chef'].sample,
-	cuisine: ['western', 'mexican', 'italian', 'brazilian'].sample,
-	meal_type: ['breakfast', 'lunch', 'dinner', 'dessert', 'snack'].sample
-	)
-end
+puts "Created Recipes!"
 
 # puts 'creating user 2'
 
