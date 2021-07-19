122b41aa83b89cd8346e34b7e11b83547b941be9
suztopp <suzannetopp81@gmail.com>
2021-07-19 15:27:38 +1000

    added more rspec testing, researching controller testing, and added in validations to dietary and ingredient tables and joiner tables

--- app/models/dietary_category.rb
+++ app/models/dietary_category.rb
@@ -1,4 +1,13 @@
 class DietaryCategory < ApplicationRecord
     has_many :recipe_dietary
-    
+
+    validates :name, presence: :true
+
+    validates :name, format: {with: /[a-zA-Z]/}, length: { minimum: 2 }
 end
+
+# create_table "dietary_categories", force: :cascade do |t|
+#     t.string "name"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
