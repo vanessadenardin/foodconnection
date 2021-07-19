19215a430914f61a009b02a0ca42bcd83397a879
Vanessa Denardin <vanessa.denardin@gmail.com>
2021-07-19 23:57:50 +1000

    added image in recipes table; set active_storage in recipe model; added user_id in create recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :dietary_categories, through: :recipe_dietaries
   has_many :ratings, dependent: :destroy
-  # has_one_attached :image
+  has_one_attached :image
   validates :recipe_name, :recipe_instructions, :cooking_time, :serves, :skill_level, :cuisine, :meal_type, :user_id, presence: :true
   validates :recipe_name, format: {with: /[a-zA-Z]/}, length: { minimum: 2 }
   validates :recipe_instructions, format: {with: /[a-zA-Z]/}, length: { minimum: 20 }
